e929da9eeb226145b8e79511ddaaafdfbe930a1f
Mateusz Paprocki <mattpap@gmail.com>
2020-07-11 23:58:38 +0200

    Migrate to shadow DOM

--- bokehjs/src/lib/models/canvas/canvas.ts
+++ bokehjs/src/lib/models/canvas/canvas.ts
@@ -179,7 +179,7 @@ export class CanvasView extends DOMView {
     ]
 
     extend(this.el.style, style)
-    append(this.el, ...elements)
+    append(this.shadow_el, ...elements)
 
     logger.debug("CanvasView initialized")
   }
